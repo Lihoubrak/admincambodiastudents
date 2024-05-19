0d8098719c257b360cee735b01a258db628e1973
lihoubrak <blh.braklihou18@gmail.com>
2024-05-20 00:19:10 +0700

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,42 +1,15 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAuj9kpi8G4wAaRkhaIbtFSRHnoaY4DfGY",
-//   authDomain: "studentapp-556ec.firebaseapp.com",
-//   databaseURL:
-//     "https://studentapp-556ec-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "studentapp-556ec",
-//   storageBucket: "studentapp-556ec.appspot.com",
-//   messagingSenderId: "970661658170",
-//   appId: "1:970661658170:web:0fea1414bea00645cb7f03",
-//   measurementId: "G-9XX7S5Z1J1",
-// };
-// const isDevelopment = process.env.NODE_ENV === "development";
-// // Initialize Firebase app
-// const app = initializeApp(firebaseConfig);
-// // Get Firestore instance
-// const db = getFirestore(app);
-// // Connect to Firestore emulator if in development environment
-// if (isDevelopment) {
-//   const firestoreEmulatorHost = "localhost";
-//   const firestoreEmulatorPort = 8080;
-//   connectFirestoreEmulator(db, firestoreEmulatorHost, firestoreEmulatorPort);
-// }
-
-// export { db };
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuj9kpi8G4wAaRkhaIbtFSRHnoaY4DfGY",
-  authDomain: "studentapp-556ec.firebaseapp.com",
-  databaseURL:
-    "https://studentapp-556ec-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "studentapp-556ec",
-  storageBucket: "studentapp-556ec.appspot.com",
-  messagingSenderId: "970661658170",
-  appId: "1:970661658170:web:0fea1414bea00645cb7f03",
-  measurementId: "G-9XX7S5Z1J1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app
